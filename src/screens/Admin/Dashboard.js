12b0af008735fdc7a42f5ef11f2f0c7cd36b2c74
Hammad Saeedi <faoqzuhair@gmail.com>
2024-05-29 02:24:26 +0500

    feat: gender and age based bar graph & introduced pie graph

--- src/screens/Admin/Dashboard.js
+++ src/screens/Admin/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import { Colors, View, ScrollView, StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
 import { Text, Card } from 'react-native-ui-lib';
-import { BarChart } from 'react-native-chart-kit';
+import { PieChart, StackedBarChart } from 'react-native-chart-kit';
 import firestore from '@react-native-firebase/firestore';
 import { useFocusEffect, useNavigation } from '@react-navigation/native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -12,10 +12,9 @@ const screenWidth = Dimensions.get('window').width;
 const DashboardScreen = () => {
   const navigation = useNavigation();
   const [studentCount, setStudentCount] = useState(0);
-  const [teacherCount, setTeacherCount] = useState(15); // Example static data for teachers
-  const revenue = 5000; // Example revenue data
-  const averageAttendance = 92; // Example average attendance percentage
-  const studentsPerClass = [30, 25, 20, 15, 30, 22, 28, 24, 26, 20]; // Example students in each class
+  const [teacherCount, setTeacherCount] = useState(0);
+  const [ageGroupsData, setAgeGroupsData] = useState([]);
+  const [studentsPerClass, setStudentsPerClass] = useState([]);
 
   const fetchStudentCount = async () => {
     try {
@@ -26,9 +25,88 @@ const DashboardScreen = () => {
     }
   };
 
+  const fetchTeacherCount = async () => {
+    try {
+      const teacherCollection = await firestore().collection('teachers').get();
+      setTeacherCount(teacherCollection.size);
+    } catch (error) {
+      console.error("Error fetching teacher count: ", error);
+    }
+  };
+
+  const fetchStudentPerClass = async () => {
+    try {
+      const studentsSnapshot = await firestore().collection('students').get();
+      const studentsData = studentsSnapshot.docs.map(doc => doc.data());
+
+      const classCounts = studentsData.reduce((acc, student) => {
+        let classID = student.class;
+        classID = (classID == 'Nursery')?0:((classID == 'Prep')?1:parseInt(classID) + 1);
+        acc[classID] = (acc[classID] || 0) + 1;
+        return acc;
+      }, []);
+
+      setStudentsPerClass(classCounts);
+    } catch (error) {
+      console.error('Error fetching student data:', error);
+    }
+  };
+
+  const fetchAgeGroupsData = async () => {
+    try {
+      const studentsCollection = await firestore().collection('students').get();
+      const studentsData = studentsCollection.docs.map(doc => doc.data());
+
+      const ageGroupsData = {
+        '0-5': { boys: 0, girls: 0 },
+        '6-10': { boys: 0, girls: 0 },
+        '11-15': { boys: 0, girls: 0 },
+        '16-20': { boys: 0, girls: 0 },
+      };
+
+      studentsData.forEach(student => {
+        const age = calculateAge(student.dob);
+        const ageGroup = age < 6 ? '0-5' : age < 11 ? '6-10' : age < 16 ? '11-15' : '16-20';
+
+        if (student.gender === 'male') {
+          ageGroupsData[ageGroup].boys++;
+        } else if (student.gender === 'female') {
+          ageGroupsData[ageGroup].girls++;
+        }
+      });
+
+      setAgeGroupsData(ageGroupsData);
+    } catch (error) {
+      console.error("Error fetching age groups data: ", error);
+    }
+  };
+
+  function calculateAge(dob) {
+    var dobArray = dob.split('-');
+    var dobDay = parseInt(dobArray[0]);
+    var dobMonth = parseInt(dobArray[1]) - 1;
+    var dobYear = parseInt(dobArray[2]);
+
+    var currentDate = new Date();
+    var currentDay = currentDate.getDate();
+    var currentMonth = currentDate.getMonth();
+    var currentYear = currentDate.getFullYear();
+
+    var age = currentYear - dobYear;
+
+    if (currentMonth < dobMonth || (currentMonth === dobMonth && currentDay < dobDay)) {
+      age--;
+    }
+
+    return age;
+  }
+
   useFocusEffect(
     useCallback(() => {
       fetchStudentCount();
+      fetchTeacherCount();
+      fetchStudentPerClass();
+      fetchAgeGroupsData();
     }, [])
   );
 
@@ -56,6 +134,46 @@ const DashboardScreen = () => {
       });
   };
 
+  const getClassLabel = (index) => {
+    switch (index) {
+      case 0:
+        return 'Nursery';
+      case 1:
+        return 'Prep';
+      case 2:
+        return 'One';
+      case 3:
+        return 'Two';
+      case 4:
+        return 'Three';
+      case 5:
+        return 'Four';
+      case 6:
+        return 'Five';
+      case 7:
+        return 'Six';
+      case 8:
+        return 'Seven';
+      case 9:
+        return 'Eight';
+      default:
+        return '';
+    }
+  }
+
+  const getClassColor = (index) => {
+    const colors = ['#ff6347', '#ffa500', '#ffd700', '#32cd32', '#4169e1', '#8a2be2', '#ff69b4', '#00ced1', '#4b0082', '#20b2aa']; // Pre-defined colors
+    return colors[index % colors.length]; 
+  };
+  
+  const pieChartData = studentsPerClass.map((value, index) => ({
+    name: getClassLabel(index),
+    population: value,
+    color: getClassColor(index),
+    legendFontColor: '#000',
+    legendFontSize: 15,
+  }));
+  
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.welcomeText}>Welcome Back!</Text>
@@ -69,43 +187,52 @@ const DashboardScreen = () => {
           <Card.Section content={[{ text: 'Total Teachers', text70: true, grey10: true }, { text: `${teacherCount}`, text60: true, green30: true }]} contentStyle={styles.cardContent} />
         </Card>
       </View>
-      <View style={styles.row}>
-        <Card style={styles.card} flex activeOpacity={1} onPress={() => alert('Manage Revenue screen')}>
-          <Card.Section content={[{ text: 'Total Revenue', text70: true, grey10: true }, { text: `$${revenue}`, text60: true, green30: true }]} contentStyle={styles.cardContent} />
-        </Card>
-        <Card style={styles.card} flex activeOpacity={1} onPress={() => alert('Average Attendance')}>
-          <Card.Section content={[{ text: 'Average Attendance', text70: true, grey10: true }, { text: `${averageAttendance}%`, text60: true, green30: true }]} contentStyle={styles.cardContent} />
-        </Card>
-      </View>
       <View style={styles.chartContainer}>
         <Text style={styles.chartTitle}>Students in Each Class</Text>
+        <PieChart
+          data={pieChartData}
+          width={screenWidth}
+          height={220}
+          chartConfig={{
+            backgroundColor: '#ffffff',
+            backgroundGradientFrom: '#ffffff',
+            backgroundGradientTo: '#ffffff',
+            color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+            style: {
+              borderRadius: 16,
+            },
+          }}
+          accessor="population"
+          backgroundColor="transparent"
+          paddingLeft="15"
+          absolute 
+        />
+      </View>
+      <View style={styles.chartContainer}>
+        <Text style={styles.chartTitle}>Total Boys and Girls in Each Age Group</Text>
         <ScrollView horizontal={true}>
-          <BarChart
+          <StackedBarChart
             data={{
-              labels: ["Class 1", "Class 2", "Class 3", "Class 4", "Class 5", "Class 6", "Class 7", "Class 8", "Class 9", "Class 10"],
-              datasets: [
-                {
-                  data: studentsPerClass
-                }
-              ]
+              labels: Object.keys(ageGroupsData),
+              legend: ['Boys', 'Girls'],
+              data: Object.values(ageGroupsData).map(data => [data.boys, data.girls]),
+              barColors: ['#3366ff', '#ff6699'], // Boys - Blue, Girls - Pink
             }}
-            width={1000} // Increased width to fit all classes
+            width={screenWidth}
             height={220}
+            yAxisSuffix=""
+            yAxisInterval={1}
             chartConfig={{
-              backgroundColor: '#3cb371',
-              backgroundGradientFrom: '#66cdaa',
-              backgroundGradientTo: '#3cb371',
+              backgroundColor: '#ffffff',
+              backgroundGradientFrom: '#ffffff',
+              backgroundGradientTo: '#ffffff',
               decimalPlaces: 0,
-              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-              labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+              labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
               style: {
-                borderRadius: 16
+                borderRadius: 16,
               },
-              propsForDots: {
-                r: "6",
-                strokeWidth: "2",
-                stroke: '#000000'
-              }
             }}
             style={styles.chart}
           />
